5f1c2427027355a94200520053fc32ce67a5c78d
李宏 <45451989+Luh1124@users.noreply.github.com>
2023-02-22 23:46:49 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-python train.py --batch_size=3 --gpu_ids=0,1,2,3,4,5,6,7 --ext=add --root_dir='../dataset/vox1/vox-png' --ckp=44
+python train.py --batch_size=3 --gpu_ids=0,1,2,3,4,5,6,7 --ext=add --root_dir='/home/momobot/repo/code/2.faceanaimation/dataset/vox1/vox-png' --ckp=44
