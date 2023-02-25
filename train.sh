e5910fabc0dbb0004d4cf19359a0013b26b5867b
Luh1124 <link0502@buaa.edu.cn>
2023-02-25 18:08:22 +0800

    train.sh

--- train.sh
+++
@@ -1,9 +0,0 @@
-# CUDA_LAUNCH_BLOCKING=1 python train.py --batch_size=2 --gpu_ids=0,1,2,3,4,5,6,7 --ext=mainv3-dl-2 --root_dir='/home/momobot/repo/code/2.faceanaimation/dataset/vox1/vox-png'
-# python train.py --batch_size=8 --gpu_ids=0,1,2,3 --ext=mainv2-1 --root_dir='/home/lh/repo/datasets/vox-png'  --ckp=1
-# python train.py --batch_size=8 --gpu_ids=0,1,2,3 --ext=mainv2-2-kpc --root_dir='/home/lh/repo/datasets/vox-png' 
-# python train.py --batch_size=8 --gpu_ids=0,1,2,3 --ext=mainv3-0 --root_dir='/home/lh/repo/datasets/vox-png' 
-# python train.py --batch_size=8 --gpu_ids=0,1,2,3 --ext=mainv3-1 --root_dir='/home/lh/repo/datasets/vox-png' 
-# python train.py --batch_size=2 --gpu_ids=0,1,2,3 --ext=mainv3-lrw --root_dir='/home/luh/repo1/datasets/face-video-preprocessing/vox-png' 
-python train.py --batch_size=2 --gpu_ids=0,1 --ext=mainv3-lrw --root_dir='/home/luh/lh_8T/datasets/LRW_Data/LRW_Temp' 
-
-
