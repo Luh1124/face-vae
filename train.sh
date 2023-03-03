df5bfcf9b6c468b987c38c6a6977cd949f2bcc4b
Luh1124 <link0502@buaa.edu.cn>
2023-03-04 03:03:40 +0800

    v9 no tanh add lkpc

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-CUDA_LAUNCH_BLOCKING=1 python train.py --batch_size=3 --gpu_ids=0,1,2,3,4,5,6,7 --ext=mainv9 --data_name='vox' --root_dir='/home/momobot/repo/code/2.faceanaimation/dataset/vox1/vox-png'
+# CUDA_LAUNCH_BLOCKING=1 python train.py --batch_size=3 --gpu_ids=0,1,2,3,4,5,6,7 --ext=mainv9 --data_name='vox' --root_dir='/home/momobot/repo/code/2.faceanaimation/dataset/vox1/vox-png'
 # python train.py --batch_size=8 --gpu_ids=0,1,2,3 --ext=mainv2-1 --root_dir='/home/lh/repo/datasets/vox-png'  --ckp=1
 # python train.py --batch_size=8 --gpu_ids=0,1,2,3 --ext=mainv2-2-kpc --root_dir='/home/lh/repo/datasets/vox-png' 
 # python train.py --batch_size=8 --gpu_ids=0,1,2,3 --ext=mainv3-0 --root_dir='/home/lh/repo/datasets/vox-png' 
@@ -6,3 +6,4 @@ CUDA_LAUNCH_BLOCKING=1 python train.py --batch_size=3 --gpu_ids=0,1,2,3,4,5,6,7 
 # python train.py --batch_size=2 --gpu_ids=0,1 --ext=mainv7_vox --data_name='vox' --root_dir='/home/luh/lh_8T/datasets/vox1/face-video-preprocessing/vox-png/' 
 # python train.py --batch_size=2 --gpu_ids=0,1 --ext=mainv7_lrw --data_name='lrw' --root_dir='/home/luh/lh_8T/datasets/LRW_Data/LRW_Temp' 
 # python train.py --batch_size=8 --gpu_ids=0,1,2,3 --ext=mainv8.2-dl10 --data_name='vox' --root_dir='/home/lh/repo/datasets/vox-png' 
+python train.py --batch_size=10 --gpu_ids=0,1,2,3 --ext=mainv9-dl5-lkpc-notanh --data_name='vox' --root_dir='/home/lh/repo/datasets/vox-png' 
