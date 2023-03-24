03d83d17185da751f9a72a99d06c060d0b18b8b1
Luh1124 <link0502@buaa.edu.cn>
2023-03-24 22:46:16 +0800

    draw_kp

--- draw_kp.py
+++ draw_kp.py
@@ -248,7 +248,7 @@ def demo(args):
                 kp_c_d = g_models["ckd"](img)
 
                 # delta = delta
-                delta_d, _, _, _, _ = g_models["efe"](img, None, kp_c_d)
+                delta_d, _, _, _, _ = g_models["efe"](img, None, kp_c)
             kp_d1 = kp_c
             kp_d2 = kp_c + delta_d
             kp_d3, Rd3 = transform_kp(kp_c + delta_d, yaw*0, pitch*0, roll*0, t*0, scale)
@@ -299,8 +299,8 @@ def demo(args):
             img_with_kp = img_with_kp.clip(0, 1)
             img_with_kp = (255 * img_with_kp).astype(np.uint8)
             # imageio.mimsave(args.output, output_frames)
-            os.makedirs(os.path.dirname(dri) + '_kp_out9notanh77d', exist_ok=True)
-            imageio.imwrite(os.path.dirname(dri) + '_kp_out9notanh77d' + '/' + f'{idx}_'+os.path.basename(dri) , img_with_kp)
+            os.makedirs(os.path.dirname(dri) + '_kp_out13v2-119', exist_ok=True)
+            imageio.imwrite(os.path.dirname(dri) + '_kp_out13v2-119' + '/' + f'{idx}_'+os.path.basename(dri) , img_with_kp)
         
         # os.makedirs(dirpath+'out', exist_ok=True)
         # out_path = os.path.join(dirpath+'out', filename)
@@ -319,7 +319,7 @@ def demo(args):
 
             img_d = vs.create_image_grid(*img_d)
             img_d = (255 * img_d).astype(np.uint8)
-            imageio.imwrite(os.path.dirname(dri) + '_kp_out9notanh77d' + '/' + f'{idx}_d_'+os.path.basename(dri), img_d)
+            imageio.imwrite(os.path.dirname(dri) + '_kp_out13v2-119' + '/' + f'{idx}_d_'+os.path.basename(dri), img_d)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="face-vid2vid")
@@ -327,9 +327,9 @@ if __name__ == "__main__":
     def str2bool(s):
         return s.lower().startswith("t")
 
-    parser.add_argument("--ckp_dir", type=str, default="ckp_1644_mainv9-dl5-lkpc-notanh", help="Checkpoint dir")
+    parser.add_argument("--ckp_dir", type=str, default="ckp_1644_main13v2_hid", help="Checkpoint dir")
     parser.add_argument("--output", type=str, default="output.gif", help="Output video")
-    parser.add_argument("--ckp", type=int, default=77, help="Checkpoint epoch")
+    parser.add_argument("--ckp", type=int, default=119, help="Checkpoint epoch")
     parser.add_argument("--source", type=str, default="./kp_s copy", help="Source image, f for face frontalization, r for reconstruction")
     parser.add_argument("--driving", type=str, default='./kp_s copy', help="Driving dir")
     parser.add_argument("--num_frames", type=int, default=90, help="Number of frames")
